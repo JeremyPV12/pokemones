6289ed7937e07ad1326ecd7d8a5f547813c5f45f
jeremy <jeremy@gmail.com>
2024-05-10 19:13:11 -0500

    cambiando

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function traerPokemon(){
-    number=10;
+    number=24;
     for (let index = 1; index<=number; index++) {
         const data= await fetch(`https://pokeapi.co/api/v2/pokemon/${index}/`)
         const response= await data.json()
@@ -18,11 +18,14 @@ async function traerPokemon(){
         img.alt=name
         div_main.append(img)
 
-
         const h1=document.createElement("h1")
         h1.innerText=name
         div_main.append(h1)
-        
+        h1.classList='flex justify-center w-full text-2xl font-medium'
+        div_main.classList='col-span-2 border border-black rounded-lg flex flex-col m-5 w-48 bg-red-100 hover:bg-black hover:text-white shadow shadow-black transition duration-1000 '
+        img.classList='w-36 h-36 flex mx-auto '
+        contenedor.classList='flex flex-wrap justify-evenly bg-red-300'
+
         contenedor.append(div_main)
     }
 }
